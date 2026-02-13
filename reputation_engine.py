1f80c27ade81116c7068be788aba7182ffa6978c
Vansh Malik <141725694+vanshmalik242@users.noreply.github.com>
2026-02-13 11:08:07 +0530

    Updated backend

---
+++ reputation_engine.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conn = sqlite3.connect("alerts.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS reputation(
+    ip TEXT PRIMARY KEY,
+    score INTEGER
+)
+""")
+
+def update_reputation(ip):
+    cursor.execute("SELECT score FROM reputation WHERE ip=?", (ip,))
+    result = cursor.fetchone()
+
+    if result:
+        new_score = result[0] + 10
+        cursor.execute("UPDATE reputation SET score=? WHERE ip=?", (new_score, ip))
+    else:
+        new_score = 10
+        cursor.execute("INSERT INTO reputation(ip, score) VALUES (?, ?)", (ip, new_score))
+
+    conn.commit()
+    return new_score
